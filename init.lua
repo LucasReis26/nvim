0cd05f3fd685ec6787b52375851f1cc1796299d0
Lucas Reis <luctreis@gmail.com>
2025-03-17 19:16:32 -0300

    migrando para o lazy.nvim

--- init.lua
+++ init.lua
@@ -1,60 +1,130 @@
-local vim = vim
-local Plug = vim.fn['plug#']
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
+end
+vim.opt.rtp:prepend(lazypath)
 
-vim.call('plug#begin')
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
 
--- Plugins
-Plug('junegunn/seoul256.vim')
-Plug('https://github.com/junegunn/vim-easy-align.git')
-Plug('fatih/vim-go', { ['tag'] = '*' })
-Plug('neoclide/coc.nvim', { ['branch'] = 'release' })
-Plug('junegunn/fzf', { ['dir'] = '~/.fzf' })
-Plug('nsf/gocode', { ['rtp'] = 'vim' })
-Plug('nvim-tree/nvim-web-devicons')
-Plug('nvim-tree/nvim-tree.lua')
-Plug('catppuccin/nvim', { ['as'] = 'catppuccin' })
-Plug('nvim-treesitter/nvim-treesitter', { ['do'] = ':TSUpdate' })
-Plug('windwp/nvim-ts-autotag')
-Plug('windwp/nvim-autopairs')
-Plug('olrtg/nvim-emmet')
-Plug('barrett-ruth/live-server.nvim')
-Plug('numToStr/Comment.nvim')
-Plug('mfussenegger/nvim-lint')
-Plug ('L3MON4D3/LuaSnip', {['tag'] = 'v2.*', ['do'] = 'make install_jsregexp'}) 
+-- Setup lazy.nvim
+require("lazy").setup({
+	spec = {
+		-- add your plugins here
+		{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+		{
+			'windwp/nvim-autopairs',
+			event = "InsertEnter",
+			config = true
+			-- use opts = {} for passing setup options
+			-- this is equivalent to setup({}) function
+		},
+		{
+			"neoclide/coc.nvim",
+			branch = "release"
+		},
+		{
+			"nvim-tree/nvim-tree.lua"
+		},
+		{
+			"nvim-tree/nvim-web-devicons"
+		},
+		{
+			"nvim-treesitter/nvim-treesitter",
+			run = ":TSUpdate"
+		},
+		{
+			'numToStr/Comment.nvim',
+			opts = {
+				-- add any options here
+			}
+		},
+		{
+			"iamcco/markdown-preview.nvim",
+			cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+			build = "cd app && yarn install",
+			init = function()
+				vim.g.mkdp_filetypes = { "markdown" }
+			end,
+			ft = { "markdown" },
+		},
+		{
+			"olrtg/nvim-emmet",
+			config = function()
+				vim.keymap.set({ "n", "v" }, '<leader>xe', require('nvim-emmet').wrap_with_abbreviation)
+			end,
+		},
+		{
+			"mikavilpas/yazi.nvim",
+			event = "VeryLazy",
+			dependencies = { "folke/snacks.nvim", lazy = true },
+			keys = {
+				-- 👇 in this section, choose your own keymappings!
+				{
+					"<leader>-",
+					mode = { "n", "v" },
+					"<cmd>Yazi<cr>",
+					desc = "Open yazi at the current file",
+				},
+				{
+					-- Open in the current working directory
+					"<leader>cw",
+					"<cmd>Yazi cwd<cr>",
+					desc = "Open the file manager in nvim's working directory",
+				},
+				{
+					"<c-up>",
+					"<cmd>Yazi toggle<cr>",
+					desc = "Resume the last yazi session",
+				},
+			},
+			---@type YaziConfig | {}
+			opts = {
+				-- if you want to open yazi instead of netrw, see below for more info
+				open_for_directories = false,
+				keymaps = {
+					show_help = "<f1>",
+				},
+			},
+			-- 👇 if you use `open_for_directories=true`, this is recommended
+			init = function()
+				-- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
+				-- vim.g.loaded_netrw = 1
+				vim.g.loaded_netrwPlugin = 1
+			end,
+		}
 
--- Outras configurações
-Plug('tpope/vim-fireplace', { ['for'] = 'clojure' })
-Plug('~/my-prototype-plugin')
-
-vim.call('plug#end')
-
--- Definir esquema de cores
-vim.g.catppuccin_flavour = "latte"  -- Aqui você define o estilo Latte
-vim.cmd('colorscheme catppuccin')  -- Aplica o esquema de cores Catppuccin Latte
-
--- Habilitar cores de 24 bits
-vim.opt.termguicolors = true
-
--- Nvim Tree
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
--- Configuração do Nvim Tree
-require("nvim-tree").setup({
-  sort = {
-    sorter = "case_sensitive",
-  },
-  view = {
-    width = 30,
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
+	},
+	-- Configure any other settings here. See the documentation for more details.
+	-- colorscheme that will be used when installing plugins.
+	install = { },
+	-- automatically check for plugin updates
+	checker = { enabled = true },
 })
 
+-- STD CONFIGS
+vim.cmd([[colorscheme catppuccin-latte]]);
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.cindent = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.guifont = "FiraCode_Nerd_Font:h14"
+vim.cmd('syntax enable')  
 local api = require('nvim-tree.api')
 vim.keymap.set('n', '<c-n>', api.tree.toggle, { noremap = true, silent = true })
 vim.keymap.set('n','<c-t>', ':split<cr><c-w>j:term<cr>:res 10<cr>i')
@@ -66,48 +136,74 @@ vim.api.nvim_create_autocmd("TermOpen",{
 })
 
 
--- Outras configurações do Neovim
-vim.o.number = true
-vim.o.relativenumber = true
-vim.o.cindent = true
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
-vim.o.guifont = "FiraCode_Nerd_Font:h14"
-vim.cmd('syntax enable')  -- Habilita a sintaxe
+-- END STD CONFIGS
 
-require('live-server').setup(config)
+-- NERD TREE CONFIGS
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 
-require('nvim-autopairs').setup({})
+-- optionally enable 24-bit colour
+vim.opt.termguicolors = true
 
-require('nvim-ts-autotag').setup({
-  opts = {
-    -- Defaults
-    enable_close = true, -- Auto close tags
-    enable_rename = true, -- Auto rename pairs of tags
-    enable_close_on_slash = false -- Auto close on trailing </
+-- empty setup using defaults
+require("nvim-tree").setup()
+
+-- OR setup with some options
+require("nvim-tree").setup({
+	sort = {
+		sorter = "case_sensitive",
+	},
+	view = {
+		width = 30,
+	},
+	renderer = {
+		group_empty = true,
+	},
+	filters = {
+		dotfiles = true,
+	},
+})
+-- END NERD TREE CONFIGS
+-- TREE SITTER CONFIGS
+require'nvim-treesitter.configs'.setup {
+  -- A list of parser names, or "all" (the listed parsers MUST always be installed)
+  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "java" },
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+
+  -- Automatically install missing parsers when entering buffer
+  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+  auto_install = true,
+
+  -- List of parsers to ignore installing (or "all")
+  ignore_install = { "javascript" },
+
+  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+
+  highlight = {
+    enable = true,
+
+    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+    -- the name of the parser)
+    -- list of language that will be disabled
+    disable = { "c", "rust" },
+    -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+    disable = function(lang, buf)
+        local max_filesize = 100 * 1024 -- 100 KB
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+            return true
+        end
+    end,
+
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
   },
-  -- Also override individual filetype configs, these take priority.
-  -- Empty by default, useful if one of the "opts" global settings
-  -- doesn't work well in a specific filetype
-  per_filetype = {
-    ["html"] = {
-      enable_close = false
-    }
-  }
-})
-
-require('Comment').setup()
-
-vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-  callback = function()
-
-    -- try_lint without arguments runs the linters defined in `linters_by_ft`
-    -- for the current filetype
-    require("lint").try_lint()
-
- end,
-})
-
-require('lint').linters_by_ft = {
-  markdown = {'vale'},
 }
